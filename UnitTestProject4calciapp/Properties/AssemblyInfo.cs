44aad1e324eef123b5e05069eb91ed960a6cf5b6
Prathima Venkatesh <prathima.2v@gmail.com>
2021-06-04 12:18:10 +0530

    Add project files.

---
+++ UnitTestProject4calciapp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject4calciapp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject4calciapp")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59a6edb3-8f93-4e2d-8657-7af594f0f21b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject4calciapp/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary4calciapp;
+
+namespace UnitTestProject4calciapp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Class1 obj = new Class1();
+        Class1 obj1 = new Class1();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            int a = 12;
+            int b = 3;
+            int actual = obj.add(a, b);
+            int expected = 15;
+            Assert.AreEqual(expected, actual);
+        }
+        public void TestMethod2()
+        {
+            int a = 4;
+            int b = 3;
+            int actual = obj1.sub(a, b);
+            int expected = 1;
+            Assert.AreEqual(expected, actual);
+
+        }
+        
+        public void TestForMultiple(int a, int b)
+        {
+            int actual = obj.add(a, b);
+            int expected = 15;
+            Assert.AreEqual(expected, actual);
+        }
+        public void TestForMultiple1(int a,int b)
+        {
+            int actual = obj1.sub(a, b);
+            int expected = 1;
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+    }
